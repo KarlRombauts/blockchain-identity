c2fc7d240fd2244b55e5dcbdd08fb9b98d6adc90
Karl Rombauts <karlrombauts@gmail.com>
2019-09-25 22:28:36 +1000

    Improve functionality of wallet and identity

--- identity.js
+++ identity.js
@@ -4,16 +4,23 @@ const MerkleTree = require('./merkleTree/Tree')
 const cryptico = require('cryptico')
 const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
-const Memory = require('lowdb/adapters/Memory')
 
-const test = false
+const EdDSA = require('elliptic').eddsa
+var ec = new EdDSA('ed25519')
 
 class Identity {
-  constructor(wallet, data, blockID) {
-    this.publicKey = wallet.pubKey
-    this.id = cryptico.publicKeyID(this.publicKey)
+  constructor(wallet, data) {
+    this.timestamp = Date.now()
+    this.walletId = wallet.id
     this.verification = {}
     this.merkleRoot = this.createMerkleTree(data).root
+
+    this.signature = {
+      key: wallet.signPublicKey,
+    }
+    this.id = SHA256(
+      this.walletId + this.timestamp + this.merkleRoot,
+    ).toString()
   }
 
   createMerkleTree(data) {
@@ -22,17 +29,52 @@ class Identity {
       item[key] = data[key]
       return item
     })
-
     const leaves = dataArray.map(item =>
       SHA256(JSON.stringify(item)).toString(),
     )
     return new MerkleTree(leaves)
   }
 
+  addVerification(verificationObject) {
+    this.verification = verificationObject
+  }
+
   checkVerification() {
-    this.verification.signature
-    this.merkleRoot
-    this.
+    if (!this.verification.signedRoot) {
+      console.log('Identity has not yet been verified')
+    }
+    const msg = SHA256(this.merkleRoot + this.verification.timestamp).toString()
+
+    const verifiers = low(new FileSync(`verifiers/db.json`))
+    const verifier = verifiers
+      .get('verifiers')
+      .find({ id: this.verification.verifierId })
+      .value()
+
+    const key = ec.keyFromPublic(verifier.signPublicKey, 'hex')
+    const isValid = key.verify(msg, this.verification.signedRoot)
+    if (isValid) {
+      console.log(`Identity has been verified by ${verifier.name}`)
+    } else {
+      console.log('Identity verification has been tampered with')
+    }
+    return isValid
+  }
+
+  sign(signature) {
+    this.signature.signedHash = signature
+  }
+
+  checkSignature() {
+    const msg = SHA256(this.id).toString()
+    const key = ec.keyFromPublic(this.signPublicKey, 'hex')
+    const isValid = key.verify(msg, this.signature.signedHash)
+    if (isValid) {
+      console.log(`Identity has been signed by wallet owner`)
+    } else {
+      console.log('Identity signature has been tampered with')
+    }
+    return isValid
   }
 }
 
--- wallet.js
+++ wallet.js
@@ -1,19 +1,22 @@
+const SHA256 = require('crypto-js/sha256')
 const Identity = require('./identity')
+const Verifier = require('./Verifier')
 const cryptico = require('cryptico')
+const EdDSA = require('elliptic').eddsa
+
 const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
-const Memory = require('lowdb/adapters/Memory')
 
+var ec = new EdDSA('ed25519')
 class Wallet {
   constructor() {
-    const nonce = Date.now() + '' + Math.random() * Math.pow(10, 18)
-    this.key = cryptico.generateRSAKey(nonce, 1024).toJSON()
-    this.pubKey = cryptico.publicKeyString(this.key)
-    this.id = cryptico.publicKeyID(this.pubKey)
-
-    const adapter = new FileSync(`wallets/${this.id}.json`)
-    this.db = low(adapter)
-    this.db.defaults({ key: {}, versions: [] }).write()
+    const nonces = this.generateKeys()
+    this.id = SHA256(this.signPublicKey).toString()
+    this.db = low(new FileSync(`wallets/private/${this.id}.json`))
+    this.db.defaults({ versions: [] }).write()
+    this.db.set('signNonce', nonces.signNonce).write()
+    this.db.set('encryptNonce', nonces.encryptNonce).write()
+    this.db.set('id', this.id).write()
   }
 
   addIdentity(data) {
@@ -21,74 +24,117 @@ class Wallet {
       .get('versions')
       .last()
       .value()
+    console.log(previousData)
+    if (previousData) {
+      const allData = { ...previousData.data, ...data }
+      data = Object.keys(allData).reduce((carry, key) => {
+        if (allData[key] !== null) {
+          carry[key] = allData[key]
+        }
+        return carry
+      }, {})
+    }
 
-    const allData = { ...previousData, ...data }
-    const newData = Object.keys(allData).reduce((carry, key) => {
-      if (allData[key] !== null) {
-        carry[key] = allData[key]
-      }
-      return carry
-    }, {})
-    console.log(newData)
-    const identity = new Identity(this, newData)
-    this.saveDataLocally(newData)
+    const identity = new Identity(this, data)
+
+    const signSecretKey = ec.keyFromSecret(this.db.get('signNonce'))
+    const msg = SHA256(
+      identity.walletId + identity.timestamp + identity.merkleRoot,
+    ).toString()
+    const signature = signSecretKey.sign(msg).toHex()
+    identity.sign(signature)
+
+    this.saveDataLocally(identity, data)
+    return identity
   }
 
-  saveDataLocally(data) {
+  saveDataLocally(identity, data) {
     this.db
       .get('versions')
-      .push(data)
+      .push({
+        walletId: this.id,
+        merkleRoot: identity.merkleRoot,
+        data: data,
+      })
       .write()
+  }
+
+  generateKeys() {
+    const encryptNonce = SHA256(
+      Date.now() + '' + Math.random() + this.id,
+    ).toString()
+    const signNonce = SHA256(
+      Date.now() + '' + Math.random() + this.id,
+    ).toString()
+    const encryptSecretKey = cryptico.generateRSAKey(encryptNonce, 1024)
+    const signSecretKey = ec.keyFromSecret(signNonce)
+    this.encryptPublicKey = cryptico.publicKeyString(encryptSecretKey)
+    this.signPublicKey = signSecretKey.getPublic('hex')
+    return { signNonce, encryptNonce }
+  }
+
+  sendForVerification(verifierId) {
+    const verifier = Verifier.getDataFromId(verifierId)
+    const message = this.db
+      .get('versions')
+      .last()
+      .value()
+
+    const encryptedData = cryptico.encrypt(
+      JSON.stringify(message.data),
+      verifier.value().encryptPublicKey,
+    )
+
+    message.data = encryptedData.cipher
+
+    verifier.value().pool.push(message)
+    verifier.write()
   }
 }
 
 const myWallet = new Wallet()
+const verifier = new Verifier('Australian Government', 'Australia')
+//TODO: encrypt data when sending to verifier
 
-myWallet.addIdentity({
+const identity = myWallet.addIdentity({
   phone: '0488300793',
   address: '23 Flowerdale Rd',
 })
 
-setTimeout(() => {
-  myWallet.addIdentity({
-    phone: '9813 8025',
-    name: 'Karl Rombauts',
-  })
-}, 1000)
+console.log('Identity created')
+identity.checkSignature()
+verifier.verifyIdentity(identity)
+identity.checkVerification()
 
-setTimeout(() => {
-  myWallet.addIdentity({
-    name: null,
-  })
-}, 2000)
-// const id1 = new Identity(
-//   wallet1,
-//   {},
-//   {
-//     phone: '0488300793',
-//     address: '23 Flowerdale Rd',
-//   },
-//   1,
-// )
+myWallet.sendForVerification(
+  '1adb66d23c2f994ae040b0df1847132caea1f7df9c38674701ffd15ce1fb5be8',
+)
 
-// const id1v2 = new Identity(
-//   wallet1,
-//   {
-//     phone: '0488300793',
-//     address: '23 Flowerdale Rd',
-//   },
-//   {
+// console.log(identity)
+
+// const identity2 = myWallet.addIdentity({
+//   name: 'Karl Rombauts',
+//   passportNo: '045534403',
+// })
+
+// console.log('Identity created')
+// identity2.checkSignature()
+// verifier.verifyIdentity(identity2)
+// identity2.checkVerification()
+
+// console.log(identity)
+
+// // console.log(identity)
+
+// setTimeout(() => {
+//   myWallet.addIdentity({
 //     phone: '9813 8025',
-//     address: '23 Flowerdale Rd',
-//   },
-//   2,
-// )
-
-// const id2 = new Identity(
-//   wallet2,
-//   {
 //     name: 'Karl Rombauts',
-//     address: '9 Degraves St',
-//   },
-//   3,
-// )
+//   })
+// }, 1000)
+
+// setTimeout(() => {
+//   myWallet.addIdentity({
+//     name: null,
+//   })
+// }, 2000)
