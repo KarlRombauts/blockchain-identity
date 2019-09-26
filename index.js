d5b663121f91de4a46d57e0f201efd69c92a8e04
Karl Rombauts <karlrombauts@gmail.com>
2019-09-27 03:36:10 +1000

    Update index.js

--- index.js
+++ index.js
@@ -3,34 +3,38 @@ const Wallet = require('./wallet')
 const Blockchain = require('./blockchain/blockchain')
 
 const blockchain = new Blockchain()
-const myWallet = new Wallet()
+const karlWallet = new Wallet()
+const jasonWallet = new Wallet()
 const vicGov = new Verifier(
   'Australian Government Victoria',
   'Australia, Victoria',
   blockchain,
 )
 
-myWallet.addIdentity({
+karlWallet.addIdentity({
   phone: '0488300793',
   address: '23 Flowerdale Rd',
 })
 
-myWallet.sendForVerification(vicGov.id)
+karlWallet.addIdentity({
+  name: 'Karl Rombauts',
+})
 
-vicGov.verifyNext()
+karlWallet.share(['phone', 'name'])
 
-blockchain.add(blockchain.mempool.next())
-blockchain.mempool.shift()
+// jasonWallet.addIdentity({
+//   name: 'jason',
+//   jobTitle: 'Developer',
+// })
 
-// setTimeout(() => {
-//   myWallet.addIdentity({
-//     phone: '9813 8025',
-//     name: 'Karl Rombauts',
-//   })
-// }, 1000)
+// karlWallet.sendForVerification(vicGov.id)
+// jasonWallet.sendForVerification(vicGov.id)
 
-// setTimeout(() => {
-//   myWallet.addIdentity({
-//     name: null,
-//   })
-// }, 2000)
+// vicGov.verifyNext()
+// vicGov.verifyNext()
+
+// blockchain.add(blockchain.mempool.next())
+// blockchain.mempool.shift()
+
+// blockchain.add(blockchain.mempool.next())
+// blockchain.mempool.shift()
